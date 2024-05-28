ca47fde1bc65a5016a6b4f65f7276bc8054cc85f
ThomasLosc <thomasthewinner62@gmail.com>
2024-05-28 12:12:02 +0200

    Ajout de fixture
    Ajout de fixture

--- src/Entity/Medecin.php
+++ src/Entity/Medecin.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MedecinRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MedecinRepository::class)]
@@ -24,6 +26,14 @@ class Medecin
 
     #[ORM\Column(length: 255)]
     private ?string $domaine = null;
+
+    #[ORM\OneToMany(targetEntity: RendezVous::class, mappedBy: 'medecin')]
+    private Collection $rendezVouses;
+
+    public function __construct()
+    {
+        $this->rendezVouses = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -74,6 +84,36 @@ class Medecin
     public function setDomaine(string $domaine): static
     {
         $this->domaine = $domaine;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, RendezVous>
+     */
+    public function getRendezVouses(): Collection
+    {
+        return $this->rendezVouses;
+    }
+
+    public function addRendezVouse(RendezVous $rendezVouse): static
+    {
+        if (!$this->rendezVouses->contains($rendezVouse)) {
+            $this->rendezVouses->add($rendezVouse);
+            $rendezVouse->setMedecin($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRendezVouse(RendezVous $rendezVouse): static
+    {
+        if ($this->rendezVouses->removeElement($rendezVouse)) {
+            // set the owning side to null (unless already changed)
+            if ($rendezVouse->getMedecin() === $this) {
+                $rendezVouse->setMedecin(null);
+            }
+        }
 
         return $this;
     }
